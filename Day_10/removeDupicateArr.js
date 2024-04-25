2327e8e7fa1058315f062abac262f630e04ce707
Raahulsharma1 <153379126+Raahulsharma1@users.noreply.github.com>
2024-04-26 00:00:06 +0530

    Add files via upload

---
+++ Day_10/removeDupicateArr.js
@@ -0,0 +1,14 @@
+
+//? Write a function that takes an array of integers an input and removes any duplicate elements, returning a new array  whit only the unique elements.
+
+const removeDuplicates = (arr) => {
+    let newArr = [...new Set(arr)]
+    // console.log(newArr);
+    return newArr
+}
+
+
+console.log(removeDuplicates([1, 1, 3, 4, 5, 9, 3, 5, 2, 6])); 
+console.log(removeDuplicates([1, 1, 2, 2, 3, 3]));
+console.log(removeDuplicates([1, 2, 3, 4]));
+console.log(removeDuplicates([]));
