7a5ab87a1057c201fa270b96c646fee2f4214cc0
Raahulsharma1 <153379126+Raahulsharma1@users.noreply.github.com>
2024-04-26 00:01:49 +0530

    removeDupicateArr.js

--- Day_10/removeDupicateArr.js
+++ Day_10/removeDupicateArr.js
@@ -5,10 +5,11 @@ const removeDuplicates = (arr) => {
     let newArr = [...new Set(arr)]
     // console.log(newArr);
     return newArr
-}
+};
 
 
 console.log(removeDuplicates([1, 1, 3, 4, 5, 9, 3, 5, 2, 6])); 
 console.log(removeDuplicates([1, 1, 2, 2, 3, 3]));
 console.log(removeDuplicates([1, 2, 3, 4]));
 console.log(removeDuplicates([]));
+
