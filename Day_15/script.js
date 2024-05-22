68dbd014ea64b742938c802ab4e95464f6b5c113
Raahulsharma1 <153379126+Raahulsharma1@users.noreply.github.com>
2024-05-22 23:26:37 +0530

    Add files via upload

---
+++ Day_15/script.js
@@ -0,0 +1,12 @@
+
+//? Write a function to calculate the sum of squares of all elements in an array. For example, given the array [1, 2, 3], the function should return 14 because 1*1 + 2*2 + 3*3 = 1 + 4 + 9 = 14.
+
+const sumOfSquares = (arr) => {
+    let sum = 0;
+    for( let elem of arr){
+        sum = sum + elem * elem;
+    }
+    return sum;
+};
+
+console.log(sumOfSquares([1, 2, 3]));
