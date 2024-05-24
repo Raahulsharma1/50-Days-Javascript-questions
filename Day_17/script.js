9f1cc51a0aa731aab5684743bc2cb1f67cc5542b
Raahulsharma1 <153379126+Raahulsharma1@users.noreply.github.com>
2024-05-24 23:57:53 +0530

    Add files via upload

---
+++ Day_17/script.js
@@ -0,0 +1,16 @@
+
+//? Write a function to convert a string to a camelCase.
+
+const toCamelCase = (str) => {
+    str = str.trim().split(" ");
+
+    str = str.map((curElem, index) => {
+        if(index === 0) {
+            return curElem.toLowerCase();
+        } else {
+            return curElem.charAt(0).toUpperCase() + curElem.slice(1).toLowerCase();
+        }
+    });
+    return str.join("")
+}
+console.log(toCamelCase("hello world raHul"));
