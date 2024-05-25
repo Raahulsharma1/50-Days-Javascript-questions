b1bd700f086e13d4f5ff00b765ed39a008f24762
Raahulsharma1 <153379126+Raahulsharma1@users.noreply.github.com>
2024-05-25 23:33:28 +0530

    Add files via upload

---
+++ Day_18/script.js
@@ -0,0 +1,11 @@
+
+//? Write a function to check if a character is uppercase or lowercase.
+
+const isUpperCase = (char) => {
+    return char === char.toUpperCase();
+    
+} 
+console.log(isUpperCase("S")); // Expected output TRUE
+
+//* Constraints:
+//? The input char will be a single character.
