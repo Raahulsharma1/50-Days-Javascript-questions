126aa219cd9466a11f3bc011541f787934ca4414
Raahulsharma1 <153379126+Raahulsharma1@users.noreply.github.com>
2024-06-01 23:35:14 +0530

    Add files via upload

---
+++ Day_24/script.js
@@ -0,0 +1,19 @@
+
+//? Write a function called findMode that takes an array of number as input and eturns the mode of the array ( The number that appears most frequently).
+
+
+function findMode(arr) {
+    let counts = {};
+    let maxNum = {};
+    let mode;
+
+    for (let element of arr) {
+        counts[element] = (counts[element] || 0) + 1;
+        if (counts[element] > maxNum);
+        maxNum= counts[element];
+        mode = element;
+    }
+    console.log(counts);
+    return mode;
+}
+console.log(findMode([1, 2, 3, 4, 6, 4, 6, 9, 6]));
