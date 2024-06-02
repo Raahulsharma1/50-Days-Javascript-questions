65146395d497cba21f61017a3c5a9db088e0ab2b
Raahulsharma1 <153379126+Raahulsharma1@users.noreply.github.com>
2024-06-02 23:37:17 +0530

    Add files via upload

---
+++ Day_25/recursion.js
@@ -0,0 +1,12 @@
+
+//? Write a function to calculate the factorial of a number. ( using recursion).
+
+// Eg. 9 => 9*8*7*6*5*4*3*2*1
+const factorial = (num) => {
+    if ( num === 1) {
+        return 1;
+    } else {
+        return num * factorial(num - 1)
+    }
+};
+console.log(factorial(9)); // Expected Output 362880
