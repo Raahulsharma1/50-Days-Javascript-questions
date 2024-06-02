d3327b038f6632e684ffcb0977914fa3969440a5
Raahulsharma1 <153379126+Raahulsharma1@users.noreply.github.com>
2024-06-02 23:37:44 +0530

    recursion.js

--- Day_25/recursion.js
+++ Day_25/recursion.js
@@ -2,6 +2,7 @@
 //? Write a function to calculate the factorial of a number. ( using recursion).
 
 // Eg. 9 => 9*8*7*6*5*4*3*2*1
+
 const factorial = (num) => {
     if ( num === 1) {
         return 1;
