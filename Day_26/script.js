967a89fa685cf706b6873de1665fcbfdaf8e8c65
Raahulsharma1 <153379126+Raahulsharma1@users.noreply.github.com>
2024-06-03 23:26:41 +0530

    Add files via upload

---
+++ Day_26/script.js
@@ -0,0 +1,14 @@
+
+//? Write a function to find the nth Fibonacci number.
+//Todo syntax: F(n) = F(n-1) + F(n-2), Where F(1) = F(2) = 1.
+
+const fibonacci = (num) => {
+    if (num <= 1) {
+        return num
+    } else {
+        return fibonacci(num - 1) + fibonacci(num - 2);
+    }
+}
+console.log(fibonacci(0)); //Expected Output: 0
+console.log(fibonacci(5)); //Expected Output: 5
+console.log(fibonacci(9)); //Expected Output: 34
